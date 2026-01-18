4f2e46db4d02e79d624c3a4a43780826893b2fe5
ashishkonale2603 <ashishkonale@gmail.com>
2026-01-18 18:05:38 +0530

    datatype defination

---
+++ 4_datatypes.py
@@ -0,0 +1,25 @@
+"""
+    Datatypes are used to specify the type and size of data that is to be stored in the variable.
+
+    Types of Dattypes:
+
+    Datatypes
+    |
+    |-Single-value Datatype(Individual Datatype)
+    | |
+    | |-Numeric
+    | | |-Integer(int)
+    | | |-Float(float)
+    | | |-Complex(comp)
+    | |
+    | |-Boolean
+    |   |-bool
+    |
+    |-Multi-value Datatype(Collection Datatype)
+      |-String(str)
+      |-List(list)
+      |-Tuple(tuple)
+      |-Set(set)
+      |-Dictionary(dict)
+
+"""
