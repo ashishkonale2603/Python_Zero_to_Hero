09da73f5247b45b04c220163d56f96edbd07cdfb
ashishkonale2603 <ashishkonale@gmail.com>
2026-01-18 18:29:42 +0530

    Default values and Imp Functions

--- 4_datatypes.py
+++ 4_datatypes.py
@@ -22,4 +22,36 @@
       |-Set(set)
       |-Dictionary(dict)
 
+      
+    Default Value:
+    Default values are the initial values that are internally considered False.
+
+    Non-default Values:
+    Non-default values are the values that are internally considered True.
+    All the values except Default Value are called as Non-default Values.
+
+    Important Functions:
+        (1)datatypeOf()[int()]
+           It is used to check the default values of specified datatype.
+           Ex. int()
+               >>0
+        
+        (2)type()
+           It is an in-built function which used to check the datatype of variable.
+           Ex. type(a)
+               >>int
+
+        (3)id()
+           It is an in-built function which used to fetch the address of values stored in the varaible.
+           Ex. id(a)
+               >>140716829472152
+
+        (4)bool()
+           It is an in-built function which used to check the values stored in the variable are default or not.
+           If it is default it will return False, if it is not default it will return True.
+           Ex. If a=10,   bool(a)
+                          >>True
+               If a=0,    bool(a)
+                          >>False
+        
 """
