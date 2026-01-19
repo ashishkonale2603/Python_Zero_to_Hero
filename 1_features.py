16dc037c104dbe9bbd86b2566ad9df8083284f0d
ashishkonale2603 <ashishkonale@gmail.com>
2026-01-19 19:45:54 +0530

    features

---
+++ 1_features.py
@@ -0,0 +1,45 @@
+"""
+    Python
+
+    Python is high-level programming language which is used to build software application.
+    It was invented by a Dutch Programmer namely Guido van Rossum in the year 1991.
+    It's name was inspired from a British Comedy show "Monty Python's Flying Circus".
+
+    Features:
+    (1) Easy to Learn and Analyse
+        Since the syntax of Python is easy for users to understand we are called so we 
+        call it as easy to learn and analyse.
+
+    (2) High-level Programming Language
+        It s easy for user to understand as well as human understandable.
+
+    
+    (3) Dynamically Typed Language
+        As we are not mentioning, the datatypes of the variables before creating it or the 
+        time of creation, we can modify the values with different datatypes at right time.
+
+    (4) Interpreted Language
+        The interpreter will check the code line by line as soon as it will find out any 
+        error at a particular line it will come out of execution by giving the error 
+        message and further lines of code will not get executed.
+
+    (5) Platform Independent Language
+        Python is not bounded any specific hardware, software, OS or browser so we call it 
+        Platform Independent Language.
+
+    (6) Open-source Language
+        It is free for all user's to download, access or to contribute.
+        We can check all the things related to Python on it's website:
+                www.python.org
+
+    (7) Huge Library Function
+        It has a huge set of library functions i.e. more then 70+ million.
+
+    (8) Multi Paradigm Language
+        Python supports multiple ways/approaches to write or solve a single problem.
+"""
+
+a, b= 1,2
+print(a+b)
+print("Hello World")
+print("Python has easy Syntax")
