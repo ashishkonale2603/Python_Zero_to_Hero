a52108c6ea13a8ddd3afa232e90bf81c5a91beef
ashishkonale2603 <ashishkonale@gmail.com>
2026-01-24 23:47:19 +0530

    slicing-shortform

--- 5_slicing.py
+++ 5_slicing.py
@@ -8,3 +8,16 @@ Slicing
 
     Slicing can be done with only those datatypes which support indexing.
 '''
+
+'''
+Shortforms for using Slicing syntax:
+    -If Starting Index is the first value we can skip writing it.
+    -If Ending Index is the last value we can skip writing it.
+    -if updation is 1 then we can skip writing it.
+    -To fetch the value of even indexing:
+        Syntax: var[ : :2]
+    -To fetch the value of even indexing:
+        Syntax: var[1: :2]
+    -To reverse the given collection we will use the
+        Syntax: var[ : :-1]
+'''
