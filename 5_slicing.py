edb0bdedcfca96263f603c09b073e94b1e3e020d
ashishkonale2603 <ashishkonale@gmail.com>
2026-01-24 23:41:13 +0530

    Slicing

---
+++ 5_slicing.py
@@ -0,0 +1,10 @@
+'''
+Slicing
+    It is a process of fetching out part of a collection in a sequential manner.
+    Syntax: var[SI:EI:Updation]
+            where SI is Starting Index
+                  EI is Ending Index
+                  Updation is number of steps
+
+    Slicing can be done with only those datatypes which support indexing.
+'''
