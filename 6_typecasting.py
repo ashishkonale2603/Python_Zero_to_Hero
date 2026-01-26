7fb01253dab99e0808d225abb97a5bbf7df2789e
ashishkonale2603 <ashishkonale@gmail.com>
2026-01-26 22:33:33 +0530

    typecast-def

---
+++ 6_typecasting.py
@@ -0,0 +1,9 @@
+'''
+Typecasting
+    It is a process of converting the datatype of a value to another datatype depending on users requirement.
+    Syntax: dest_var = dest_dt(source_var)
+    Ex.
+'''
+source=5
+dest=float(source)
+print(dest)
