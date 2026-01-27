dff62317076de477b646b9aa9511e27bdb95e1a7
ashishkonale2603 <ashishkonale@gmail.com>
2026-01-27 23:15:56 +0530

    defination

---
+++ 7_copy_operation.py
@@ -0,0 +1,10 @@
+'''
+Copy Operation:
+
+    The process of coping the content from one variable to another is called as copy operation.
+
+    There are 3 types of copy:
+    (1) General Copy
+    (2) Shallow Copy
+    (3) Deep Copy
+'''
