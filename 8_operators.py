38f90af1b99f1399dfc5232b502a48e470e75beb
ashishkonale2603 <ashishkonale@gmail.com>
2026-01-28 23:42:43 +0530

    def

--- 8_operators.py
+++ 8_operators.py
@@ -1,3 +1,20 @@
 '''
+Operators:
+    Operators are the special symbols or keywords which are used to perform some specific task(predefined task)
+    by the developer.
 
+    Operators are one of the type of library function.
+
+    Operands:
+    Operands are the values on which operator will perform the task.
+    Number of operands required to perform a specific task depends upon the type of operator which is to be used.
+
+    We have 7 types of operator:
+    (a) Arithmatic Operator
+    (b) Relational Operator
+    (c) Logical Operator
+    (d) Bitwise Operator
+    (e) Assignment Operator
+    (f) Identity Operator
+    (g) Membership Operator
 '''
