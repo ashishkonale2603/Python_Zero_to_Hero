aac1d07a2d9d848be5feabbb9c4d36f468e69b23
ashishkonale2603 <ashishkonale@gmail.com>
2026-01-28 22:46:43 +0530

    general-copy

--- 7_copy_operation.py
+++ 7_copy_operation.py
@@ -8,3 +8,27 @@ Copy Operation:
     (2) Shallow Copy
     (3) Deep Copy
 '''
+
+'''
+General Copy:
+It is the process of coping the content from one variable to another with same address.
+Syntax: dest_var = source_var
+
+Drawback:
+In general copy, if values from one variable is modified another variable as well.
+
+Ex.
+'''
+a=[1,2,[3,4,7]]
+print(a)
+
+b=a
+print(b)
+
+a[1]=8
+print(a)
+print(b)
+
+a[2][1]=17
+print(a)
+print(b)
