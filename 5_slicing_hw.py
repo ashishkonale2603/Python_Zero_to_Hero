d8b49c2ff2797f518a005690cad51bc7649eba7b
ashishkonale2603 <ashishkonale@gmail.com>
2026-02-01 23:45:58 +0530

    str1

---
+++ 5_slicing_hw.py
@@ -0,0 +1,19 @@
+str1 = "abcdefghijklmnopqrstuvwxyz"
+
+print(str1[2::3])
+#cfilorux
+
+print(str1[20:4:-1])
+#utsrqponmlkjihgf
+
+print(str1[:5]+str1[20:4:-1]+str1[21:])
+#abcdeutsrqponmlkjihgfvwxyz
+
+a=len(str1)//2
+print(str1[a::2])
+#nprtvxz
+
+print(str1[20:4:-1])
+#utsrqponmlkjihgf
+
+print()
