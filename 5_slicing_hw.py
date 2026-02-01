48db63caa4b39d7e1a709a411b6d4b94c0c7238e
ashishkonale2603 <ashishkonale@gmail.com>
2026-02-01 23:48:59 +0530

    str3

--- 5_slicing_hw.py
+++ 5_slicing_hw.py
@@ -17,3 +17,23 @@ print(str1[20:4:-1])
 #utsrqponmlkjihgf
 
 print()
+
+
+str3='datastructuresandalgorithms'
+
+print(str3[0:4:]+str3[16:3:-1]+str3[17::])
+#datadnaserutcurtsalgorithms
+
+print(str3[5:-9:1])
+#tructuresanda
+
+print(str3[::-4])
+#silaurt
+
+print(str3[0:3:1]+str3[-4:2:-1]+str3[24::])
+#dattirogladnaserutcurtsahms
+
+print()
+#
+
+print()
