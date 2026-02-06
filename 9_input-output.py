46f71a8320b64dae3609952c8fd6d05fc2e11e7c
ashishkonale2603 <ashishkonale@gmail.com>
2026-02-06 21:46:31 +0530

    input

---
+++ 9_input-output.py
@@ -0,0 +1,40 @@
+
+#Input/Output
+'''
+Input:
+
+    The statements which are used to take data in a form of input from user are called as input statement.
+    We use input in-built function to take the data from the user.
+    It is a in-built function which is used to take values from the user to make the code dynamic.
+
+    Syntax:  var_name = input('message')
+                                  ^
+                                  |
+                                  |-----optional
+    
+    Message in the in-built function is optional.
+    Whenever we are using input function, data will be stored in the form of String.
+    To take the data from the user in a specified datatype, it is required to typecast.
+
+    Ex.
+        a=input('Enter no:')
+        
+
+        ----------------------------------------------------------------------------------------------------------------------
+        |--------------------------------------------------------------------------------------------------------------------|
+        |                                                                                                                    |
+        |   eval()                                                                                                           |
+        |   It is an in-built function which is used to evaluate the input datatype and store in the form of same datatype.  |
+        |   Syntax:  var_name = eval(input('message'))                                                                       |
+        |                                                                                                                    |
+        |   weight=eval(input('Enter Weight:'))                                                                              |
+        |   print(weight, type(weight))                                                                                      |
+        |                                                                                                                    |
+        |   Ex.                                                                                                              |
+        |           10                              2.3                                                                      |
+        |           >>10,int                        >>2.3,float                                                              |
+        |                                                                                                                    |
+        |--------------------------------------------------------------------------------------------------------------------|
+        ----------------------------------------------------------------------------------------------------------------------
+''' 
+
