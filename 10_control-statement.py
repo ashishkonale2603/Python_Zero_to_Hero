b26fe958a83813f102579fa26142d31165c02d71
ashishkonale2603 <ashishkonale@gmail.com>
2026-02-06 23:26:43 +0530

    control-statement

---
+++ 10_control-statement.py
@@ -0,0 +1,18 @@
+#Control Statement
+
+'''
+    Control Statement are used to control the flow of execution of the given program.
+
+    It is further divided into two parts:
+
+                                Control Statement
+                                        |                       l                   
+                -------------------------------------------------
+                |                                               |
+    Conditional Statement                               Looping Statement
+        |                                                   |
+        |-Simple if                                         |-while
+        |-if else                                           |
+        |-elif                                              |-for
+        |-Nested if
+'''
