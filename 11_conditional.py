b8ff36d63a116b7f474ed21c8c99a13b0da6a10e
ashishkonale2603 <ashishkonale@gmail.com>
2026-02-09 20:22:03 +0530

    simple-if

---
+++ 11_conditional.py
@@ -0,0 +1,45 @@
+'''
+Conditional Statements
+    These are the statements which are used to control the flow of execution of a given program.
+'''
+
+'''
+(1)Simple if
+    It is a type of conditional statement where we have one condition to be satisfied and one statement block to be executed.
+    It the condition becomes it will be executed True statement block or else it will ignore the true statement block.
+
+    Syntax:
+            if condition:
+                True Statement Block
+
+    Flowchart:
+                                |
+                                | 
+                    ___________ if
+                    |          cond
+                    |           |
+                    |           |
+                    |    True Statement Block
+                    |           |
+
+    Ex.
+'''
+# (1)
+# a=int(input('Enter number for square:'))
+# if a%2==0:
+#     print(a*a)
+
+# (2)
+# b=input('Enter a single character:')
+# if b in "aeiouAEIOU":
+#     print(b)
+
+# (3)
+# c=input('Enter a single character:')
+# if 'A' <= c <= 'Z':
+#     print(ord(c))
+
+
+'''
+
+'''
