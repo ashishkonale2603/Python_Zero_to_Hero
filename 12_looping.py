3018f87c7dfa5fc287453e46ecbb0336a49349c5
ashishkonale2603 <ashishkonale@gmail.com>
2026-02-09 21:07:14 +0530

    loop

---
+++ 12_looping.py
@@ -0,0 +1,8 @@
+'''
+Looping Statement
+    Looping Statement are the types of control statements which is used to control the flow of execution by repeating the task again and again for specified n number of times.
+
+    We have two types of looping it:
+    (i)while loop
+    (ii)for loop
+'''
