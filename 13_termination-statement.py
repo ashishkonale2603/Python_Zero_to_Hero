a765ccdcea9aa06d0cb83e1121a43fa3f7257674
ashishkonale2603 <ashishkonale@gmail.com>
2026-02-17 22:20:04 +0530

    continue

--- 13_termination-statement.py
+++ 13_termination-statement.py
@@ -21,3 +21,14 @@ Ex.
 O/P:    1,2        
 '''
 
+'''
+(ii)continue
+It is used to skip the iteration here furthur lines of code will not get executed but it will go back to the loop again.
+Ex.
+    for i in range(1,6):
+        if i==3:
+            continue
+        print(i)
+
+O/P:    1,2,4,5
+'''
