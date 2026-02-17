6424d09500336251471a58753467115d06c88c33
ashishkonale2603 <ashishkonale@gmail.com>
2026-02-17 22:19:37 +0530

    break

---
+++ 13_termination-statement.py
@@ -0,0 +1,23 @@
+'''
+Intermediate Termination Statement:
+    This statement are used to terminate the loop based on user requirement.
+
+    We have three types of keywords:
+    (i)break
+    (ii)continue
+    (iii)pass
+'''
+
+'''
+(i)break
+It is the intermediate termination which is used to stop once it is executed it will come out of the loop and it will not
+run furthur lines of code also it will not go back to the loop again.
+Ex.
+    for i in range(1,6):
+        if i==3:
+            break
+        print(i)
+
+O/P:    1,2        
+'''
+
