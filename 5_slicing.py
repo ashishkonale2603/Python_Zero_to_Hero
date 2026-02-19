f511f9c469fd9ca40db136a30aea84d96fd22d86
ashishkonale2603 <ashishkonale@gmail.com>
2026-02-19 22:33:33 +0530

    Enhance documentation on slicing in Python
    Added notes on slicing syntax and its usage.

--- 5_slicing.py
+++ 5_slicing.py
@@ -8,6 +8,7 @@ Slicing
 
     Slicing can be done with only those datatypes which support indexing.
 
+
     
 Shortforms for using Slicing syntax:
     -If Starting Index is the first value we can skip writing it.
