527fb9bd1723c081ebeab477ee0fe86708b01dca
ashishkonale2603 <ashishkonale@gmail.com>
2026-02-21 22:04:52 +0530

    in-built

--- 14_functions.py
+++ 14_functions.py
@@ -6,3 +6,15 @@ Function:
     (1)In-build functions
     (2)User defined functions
 '''
+
+'''
+(1)In-built functions
+    The function which are predefined by the developer are called as in-built functions.
+    We have 6 types of in-built functions:
+    (i)Utility functions
+    (ii)Functions on String
+    (iii)Functions on List
+    (iv)Functions on Tuple
+    (v)Functions on Set
+    (vi)Functions on Dictionary
+'''
