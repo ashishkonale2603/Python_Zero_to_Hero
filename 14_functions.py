e0b52449fa61e46031d38fcd4f2f9a95a738b65e
ashishkonale2603 <ashishkonale@gmail.com>
2026-02-21 21:59:41 +0530

    function

---
+++ 14_functions.py
@@ -0,0 +1,8 @@
+'''
+Function:
+    Functions are the set of instructions or blocks of code which are used to perform some specific task or operation.
+
+    We have 2 types of functions:
+    (1)In-build functions
+    (2)User defined functions
+'''
