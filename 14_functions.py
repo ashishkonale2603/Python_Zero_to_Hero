7261a59315865ca34d8399a003afed26d2569027
ashishkonale2603 <ashishkonale@gmail.com>
2026-02-26 23:01:31 +0530

    Update 14_functions.py

--- 14_functions.py
+++ 14_functions.py
@@ -47,4 +47,10 @@ Function:
     To check with all the in-built functions which support set datatypes we can use dir(set).
     Ex.
         union(), issubset(), issuperset(), add(), pop()
+
+    (vi)Functions on dictionary
+    The in-built functions which supports dictionary datatypes are called as Functions on dictionary.
+    To check with all the in-built functions which support dictionary datatypes we can use dir(dictionary).
+    Ex.
+        keys(), values(), items(), popitem()
 '''
