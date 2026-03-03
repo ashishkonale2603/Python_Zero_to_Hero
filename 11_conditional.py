315a0aced8e799442cb3555e8fc88c5adcd3f4bc
ashishkonale2603 <ashishkonale@gmail.com>
2026-03-03 20:03:44 +0530

    Fix formatting and syntax in simple if statement
    Correct syntax and formatting in the simple if statement explanation.

--- 11_conditional.py
+++ 11_conditional.py
@@ -7,7 +7,6 @@ Conditional Statements
 (1)Simple if
     It is a type of conditional statement where we have one condition to be satisfied and one statement block to be executed.
     It the condition becomes it will be executed True statement block or else it will ignore the true statement block.
-
     Syntax:
             if condition:
                 True Statement Block
