1f20ef6095d5b81f3dad8c7f9d9342970504a7fe
ashishkonale2603 <ashishkonale@gmail.com>
2026-03-26 23:10:37 +0530

    arguments

---
+++ 15_arguments.py
@@ -0,0 +1,26 @@
+# Types of Arguments
+
+'''
+We have four types of arguments:
+(1)Positional Argument
+(2)Keyword Argument
+(3)Default Argument
+(4)Variable Length Argument
+
+'''
+
+'''
+(1)Positional Argument
+    Positional arguments are the arguments which are used to store the value inside the formal arguments depending upon the position.
+    It stores values based on position.
+    Positional arguments are the first argument written inside a function.
+    Ex.
+        def fname(name,roll):
+            print(name,roll)
+        fname('ashish',27)
+
+'''
+
+
+
+
