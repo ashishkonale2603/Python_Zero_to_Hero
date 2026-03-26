0f446da845c75ddca61b8fb1a2a3a474e8bd17da
ashishkonale2603 <ashishkonale@gmail.com>
2026-03-26 23:27:49 +0530

    pack

---
+++ 16_packing.py
@@ -0,0 +1,18 @@
+# Packing and Unpacking
+
+'''
+Packing:
+
+It is the process of combining the values into a collection.
+
+Wrapping up the data/values togetherly inside a collection is called as Packing.
+It can be done by using all the five collection datatypes.
+
+There are two types of packing:
+(1)Single Pack
+(2)Double Pack
+'''
+
+'''
+
+'''
