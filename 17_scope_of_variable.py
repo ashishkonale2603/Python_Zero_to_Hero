990c0a325af259007e00207e8d9eafcdaf55b328
ashishkonale2603 <ashishkonale@gmail.com>
2026-03-30 21:35:37 +0530

    nonlocal variable

--- 17_scope_of_variable.py
+++ 17_scope_of_variable.py
@@ -23,3 +23,24 @@ def add():
     a=20
 add()
 print(a)
+
+'''
+(2)Local Variables
+    -The variables which are declared inside a function is called as local variable.
+    -The scope of local variable is from starting of the function to the end of the function.
+    -We cannot access the local variable outside the function.
+    -We can access the modify the local variable inside the function.
+    -When we are using nested function we can only access the local variable but cannot modify it.
+    -To modify the local variable inside the nested function we use the keyword 'nonlocal'.
+
+    Ex.
+'''
+def ex1():
+    a=10
+    print(a)
+    def ex2():
+        nonlocal a
+        a=20
+    ex2()
+    print(a)
+ex1()
