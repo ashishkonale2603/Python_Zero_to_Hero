58a585911fbfd95cf1d7e22baa9d75d51f462279
ashishkonale2603 <ashishkonale@gmail.com>
2026-03-30 22:14:53 +0530

    recurrsion

---
+++ 18_recurrsion.py
@@ -0,0 +1,14 @@
+# Recurrsion
+
+'''
+    It is the process of function calling inself n number of times unless and until the termination condition becomes true.
+
+    Syntax:
+            def fname():
+                if termination cond:
+                    return val
+                else:
+                    return fname()
+            fname()
+
+'''
