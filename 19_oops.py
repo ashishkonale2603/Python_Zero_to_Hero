a2ea88ce9ee472fdec5f4d7758fd73ca614d1677
ashishkonale2603 <ashishkonale@gmail.com>
2026-03-31 21:01:56 +0530

    oops

---
+++ 19_oops.py
@@ -0,0 +1,27 @@
+# OOPS
+
+'''
+    OOPS stands for Object Oriented Programming System.
+
+    It is an approach which is used to solve real world problems in virtual way(by creating software) with use of classes and objects.
+
+        Class:
+        It is a blueprint or template which consist of properties and functionalities of an object.
+
+        Object:
+        It is a real-time entity or an instance of a class.
+
+    Syntax:
+        class class_name:
+            properties and functionalities
+
+        object_name=classname(args)
+
+    Ex.
+'''
+class Car:
+    car_brand='Tesla'
+    car_model='Model S'
+    fuel_type='Electric'
+car1=Car()
+car2=Car()
